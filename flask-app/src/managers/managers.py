9c9b0ec35640621b1006cce3959b664dde1fb031
Ryan Lake <73082037+RyanLake6@users.noreply.github.com>
2023-04-18 18:56:04 -0400

    data added

--- flask-app/src/managers/managers.py
+++ flask-app/src/managers/managers.py
@@ -25,17 +25,17 @@ managers = Blueprint('managers', __name__)
 @managers.route('/maintanence/<callNum>', methods=['GET'])
 def get_maintanence_datetimes(callNum):
     cursor = db.get_db().cursor()
-    # cursor.execute('FILL IN')
-    # row_headers = [x[0] for x in cursor.description]
-    # json_data = []
-    # theData = cursor.fetchall()
-    # for row in theData:
-    #     json_data.append(dict(zip(row_headers, row)))
-    # the_response = make_response(jsonify(json_data))
-    # the_response.status_code = 200
-    # the_response.mimetype = 'application/json'
-    return "hit this endpoint"
-    # return the_response
+    cursor.execute("SELECT * FROM Inventory")
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    # return "hit this endpoint"
+    return the_response
 
 
 # get request for maintanence table based on datetime
