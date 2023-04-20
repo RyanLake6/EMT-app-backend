8734f31d3cd0f01942dc19ffcd984566650f7733
Ryan Lake <73082037+RyanLake6@users.noreply.github.com>
2023-04-19 21:58:15 -0400

    everything working except travel times

--- flask-app/src/managers/managers.py
+++ flask-app/src/managers/managers.py
@@ -9,7 +9,7 @@ managers = Blueprint('managers', __name__)
 @managers.route('/test', methods=['GET'])
 def test():
     cursor = db.get_db().cursor()
-    cursor.execute("select * from Maintenance")
+    cursor.execute("SELECT TIMEDIFF('2000:01:01 00:00:00', '2000:01:01 00:00:00.000001')")
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -187,6 +187,74 @@ def get_inventory(runNumber):
             FROM Inventory join Truck using(truckID) join Emergency using (truckID) where Emergency.runNumber=%s
             '''
     args = (runNumber)
+    cursor.execute(query, args)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+
+# Should return all survey avg rating data based on emplpyee id
+@managers.route('/SurveyAvg/<employeeID>', methods=['GET'])
+def get_survey_avg(employeeID):
+    cursor = db.get_db().cursor()
+    query = '''
+          SELECT Survey.rating, COUNT(*) as count
+          from Survey join Patient using (MRN) join Emergency using (runNumber) 
+          join EmergencyEmployee using (runNumber) join Employees using (employeeID) 
+          where Employees.employeeID=%s
+          GROUP BY Survey.rating
+            '''
+    args = (employeeID)
+    cursor.execute(query, args)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+
+# Should return all travel times avg info based on the employee id
+@managers.route('/travelTimes/<employeeID>', methods=['GET'])
+def get_travel_times(employeeID):
+    cursor = db.get_db().cursor()
+    query = '''
+          SELECT *
+          from Times join Emergency using (runNumber) join EmergencyEmployee using (runNumber) join Employees using (employeeID) where Employees.employeeID=%s
+            '''
+    args = (employeeID)
+    cursor.execute(query, args)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+# Should return number of emergencies employee did per day
+@managers.route('/emergencyNumbers/<employeeID>', methods=['GET'])
+def get_emergency_numbers(employeeID):
+    cursor = db.get_db().cursor()
+    query = '''
+          SELECT DAY(Times.dispatched) as day, COUNT(*) as count from Emergency join EmergencyEmployee using (runNumber) join Employees using (employeeID) join Times using (runNumber) where Employees.employeeID=%s
+          GROUP BY DAY(Times.dispatched )
+            '''
+    args = (employeeID)
     cursor.execute(query, args)
     row_headers = [x[0] for x in cursor.description]
     json_data = []
